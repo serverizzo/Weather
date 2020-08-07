737fc7437f273e00c6abc8dd797cf5aeb0bef303
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-08-07 14:44:52 -0400

    partially filled accordian

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,7 @@
 const tenMilesInMeters = 16093;
 
 // setup map
-var mymap = L.map('mapid').setView([51.505, -0.09], 2);
+var mymap = L.map('mapid').setView([51.505, -0.09], 3);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
