5dd24c3d508896aacf6956d49fdfbf19717bee8d
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-08-07 17:26:02 -0400

    added circle to center of the map when changing the coordinates

--- public/sketch.js
+++ public/sketch.js
@@ -21,120 +21,59 @@ function updatePleaseWait() {
 function setup() {
     noCanvas();
     geolocate("first")
+
 }
 
 
-async function geolocateHere(myInput) {
-    if ('geolocation' in navigator) {
-        console.log("Geolocation avalible. Please wait for location.")
+let isOn = false
+let circle;
+let centerFunction = function () {
+    circle.setLatLng(mymap.getCenter())
+    console.log("I moved!")
+}
 
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            // console.log(position);
+function toggleCircle() {
 
-            const ll = mymap.getCenter()
-            console.log(ll)
-            const latitude = ll.lat;
-            const longitude = ll.lng;
-
-            // toFixed truncates the decimal
-            // const latitude = position.coords.latitude.toFixed(3)
-            // const longitude = position.coords.longitude.toFixed(3)
-
-            //update html
-            document.getElementById("lon").textContent = longitude
-            document.getElementById("lat").textContent = latitude
-            // console.log(longitude)
-            // console.log(latitude)
-
-
-            // request weather from server endpoint
-            const latlon = `${latitude},${longitude}`
-            const weather_response = await fetch(`/weather/${latlon}`)
-            const weatherInfojson = await weather_response.json()
-            // console.log(weatherInfojson)
-
-            // update weather
-            document.getElementById("weatherBase").textContent = weatherInfojson.name;
-            document.getElementById("weather").textContent = weatherInfojson.weather[0].main;
-            document.getElementById("tempature").textContent = weatherInfojson.main.temp;
-            document.getElementById("feelsLike").textContent = weatherInfojson.main.feels_like;
-
-
-            // request air quality from server endpoint
-            const aq_response = await fetch(`/aq/${latlon}`)
-            const aq_json = await aq_response.json();
-            console.log(aq_json)
-
-            // put data in database
-            const data = {
-                latitude,
-                longitude,
-                weatherInfojson,
-                aq_json
-            }
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            }
-            const response = await fetch('/api', options)
-            const json = await response.json();
-            console.log(json)
-
-
-            //  change zoom and position of map
-            // mymap.setZoom
-            mymap.setView([latitude, longitude], 10)
-
-            var marker = L.marker([latitude, longitude]).addTo(mymap);
-            marker.bindPopup("You are here.").openPopup();
-            var circle = L.circle([latitude, longitude], {
-                color: 'red',
-                fillColor: '#f03',
-                fillOpacity: 0.5,
-                radius: tenMilesInMeters
-            }).addTo(mymap);
-
-            const weatherStationIcon = L.icon({
-                iconUrl: "./icons/icons8-spring-100.png"
-            })
-
-            for (let i = 0; i < aq_json.results.length; i++) {
-                const curr = aq_json.results[i]
-                const aqNode = L.marker([curr.coordinates.latitude, curr.coordinates.longitude], {
-                    icon: weatherStationIcon
-                }).addTo(mymap);
-
-                let measurements = "";
-                for (let j = 0; j < curr.measurements.length; j++) {
-                    measurements +=
-                        `<br>
-                        Pollutant: ${curr.measurements[j].parameter} <br>
-                        Value: ${curr.measurements[j].value} ${curr.measurements[j].unit}  <br>
-                        Updates: ${curr.measurements[j].lastUpdated} <br>
-                        Source: ${curr.measurements[j].sourceName} <br>
-                        `
-                }
-
-                const popupContent = aqNode.bindPopup(`<p>
-                    City: ${curr.city}, <br>
-                    Coordinates: ${curr.coordinates.latitude}, ${curr.coordinates.longitude} <br>
-                    Location: ${curr.location} <br>
-                    Measurements: <br> ${measurements}
-                    </p>`)
-            }
-
-            updatePleaseWait()
-
-        })
-    } else {
-        console.log("Geolocation unavalible")
+    if (isOn) {
+        deleteCircle()
+        geolocate()
+        document.getElementById("geolocateButtonText").innerHTML = "Change my coordinates"
     }
-
-    console.log("Button function completed.");
+    else {
+        // create visual indicator
+        makeCircle()
+        // change button text to geolocate
+        document.getElementById("geolocateButtonText").innerHTML = "Geolocate here"
+    }
+    isOn = !isOn
 }
+
+function deleteCircle() {
+    // remove event listener for movement
+    mymap.off("move", centerFunction)
+    // remove circle
+    circle.remove()
+    console.log("removed listener")
+}
+
+function makeCircle() {
+    const ll = mymap.getCenter()
+    console.log(ll)
+    latitude = ll.lat;
+    longitude = ll.lng;
+
+    circle = L.circle(mymap.getCenter(), {
+        color: '#9000ff',
+        fillColor: '#9000ff',
+        fillOpacity: 0.5,
+        radius: tenMilesInMeters
+    }).addTo(mymap);
+
+
+    // add event listener for movement (panning)
+    mymap.on("move", centerFunction)
+}
+
 
 
 
